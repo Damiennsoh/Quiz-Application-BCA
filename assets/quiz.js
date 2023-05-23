0a3eca3f178d229c5d38450e6ac8321412fcf175
Damiennsoh <psycaro20@gmail.com>
2023-05-24 02:48:25 +0530

    modified the quiz.js and the HTML as well as the CSS coded files

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,10 +1,253 @@
 // Define the questions and answers
-const questions = [  {    question: "What is the capital of France?",    answers: {      a: "Paris",      b: "Madrid",      c: "Rome",    },    correctAnswer: "a",  },  
-{    question: "What is the largest planet in our solar system?",    answers: {      a: "Jupiter",      b: "Saturn",      c: "Neptune",    },    correctAnswer: "a",  },  
-{    question: "What is the smallest country in the world?",    answers: {      a: "Monaco",      b: "Vatican City",      c: "San Marino",    },    correctAnswer: "b",  },
-{    question: "Where is MMDU located in India?", answers: { a: "Chandigah", b: "Delhi", c: "Mullana", }, correctAnswer: "c",   },
-{    question: "What is the full meaning of BCA as a course in MMDU?", answers: {a: "Bachelor of Computer Applications", b: "Bachelor of Cardiac Arrest", c: "Bachelor of Computer Science"}, correctAnswer: "a", },
-{    question: "Who is the head of the BCA department?", answers: { a: "Vivek Bhatnagar", b: "Gautam Kumar", c: "Dr Rattan", }, correctAnswer: "a", },
+const questions = [  
+{    
+  question: "What is the capital of France?",    
+  answers: {      
+    a: "Paris",      
+    b: "Madrid",      
+    c: "Rome",    
+  },    
+  correctAnswer: "a",  
+},  
+{    
+  question: 
+  "What is the largest planet in our solar system?",    
+  answers: {      
+    a: "Jupiter",      
+    b: "Saturn",      
+    c: "Neptune",    
+  },    
+  correctAnswer: "a",  
+},  
+{    
+  question: "What is the smallest country in the world?",    
+  answers: {      
+    a: "Monaco",      
+    b: "Vatican City",      
+    c: "San Marino",   
+},    
+correctAnswer: "b",  
+},
+{    
+  question: "Where is MMDU located in India?", 
+  answers: { 
+    a: "Chandigah", 
+    b: "Delhi", 
+    c: "Mullana", 
+  }, 
+  correctAnswer: "c",   
+},
+{    
+  question: "What is the full meaning of BCA as a course in MMDU?", 
+  answers: {
+    a: "Bachelor of Computer Applications", 
+    b: "Bachelor of Cardiac Arrest", 
+    c: "Bachelor of Computer Science",
+  }, 
+  correctAnswer: "a", 
+},
+{    
+  question: "Who is the head of the BCA department?", 
+  answers: { 
+    a: "Vivek Bhatnagar", 
+    b: "Gautam Kumar", 
+    c: "Dr Rattan Rana",
+  }, 
+  correctAnswer: "a", 
+},
+{
+
+// Questions about HTML
+  question: "What does HTML stand for?",
+  answers: {
+  a: "Hyper Text Markup Language",
+  b: "Hyperlinks and Text Markup Language",
+  c: "Home Tool Markup Language",
+  },
+  correctAnswer: "a",
+  },
+  {
+  question: "What is the basic structure of an HTML document?",
+  answers: {
+  a: "DOCTYPE, head, body",
+  b: "head, body",
+  c: "DOCTYPE, title, head, body",
+  },
+  correctAnswer: "c",
+  },
+  {
+  question: "What is the purpose of the &lt;head&gt; tag in HTML?",
+  answers: {
+  a: "To define the main content of the webpage",
+  b: "To define the title and metadata of the webpage",
+  c: "To define a section of the webpage that contains navigation links",
+  },
+  correctAnswer: "b",
+  },
+  {
+  question: "What is the purpose of the &lt;img&gt; tag in HTML?",
+  answers: {
+  a: "To display an image on the webpage",
+  b: "To create a hyperlink to another webpage",
+  c: "To define a section of the webpage that contains text",
+  },
+  correctAnswer: "a",
+  },
+  {
+  question: "What is the purpose of the &lt;a&gt; tag in HTML?",
+  answers: {
+  a: "To create a hyperlink to another webpage or a specific location within the same webpage",
+  b: "To display an image on the webpage",
+  c: "To define a section of the webpage that contains text",
+  },
+  correctAnswer: "a",
+  },
+  {
+  question: "What is the purpose of the &lt;div&gt; tag in HTML?",
+  answers: {
+  a: "To define a section of the webpage that contains navigation links",
+  b: "To define a section of the webpage that contains text",
+  c: "To group together other HTML elements and apply styles to them",
+  },
+  correctAnswer: "c",
+  },
+  {
+  question: "What is the purpose of the &lt;table&gt; tag in HTML?",
+  answers: {
+  a: "To define a section of the webpage that contains text",
+  b: "To create a list of items",
+  c: "To display data in rows and columns",
+  },
+  correctAnswer: "c",
+  },
+  {
+  question: "What is the purpose of the &lt;form&gt; tag in HTML?",
+  answers: {
+  a: "To create a list of items",
+  b: "To display an image on the webpage",
+  c: "To create a user input area for submitting data to a server",
+  },
+  correctAnswer: "c",
+  },
+  {
+  question: "What is the purpose of the &lt;ul&gt; and &lt;ol&gt; tags in HTML?",
+  answers: {
+  a: "&lt;ul&gt; is used to create an unordered list and &lt;ol&gt; is used to create an ordered list",
+  b: "&lt;ul&gt; is used to create an ordered list and &lt;ol&gt; is used to create an unordered list",
+  c: "Both &lt;ul&gt; and &lt;ol&gt; are used to create unordered lists",
+  },
+  correctAnswer: "a",
+  },
+  {
+  question: "What is the purpose of the &lt;header&gt; and &lt;footer&gt; tags in HTML?",
+  answers: {
+  a: "&lt;header&gt; is used to define a section of the webpage that contains navigation links and &lt;footer&gt; is used to define a section that contains copyright information",
+  b: "&lt;header&gt; is used to define the main content of the webpage and &lt;footer&gt; is used to define a section of the webpage that contains navigation links",
+  c: "&lt;header&gt; is used to define a section of the webpage that contains the title and metadata of the webpage and &lt;footer&gt; is used to define the main content of the webpage",
+  },
+  correctAnswer: "a",
+  },
+  
+  // Questions for Java programming language
+  {
+    question: "What is Java?",
+    answers: {
+      a: "A type of coffee",
+      b: "A programming language",
+      c: "A type of computer",
+    },   
+    correctAnswer: "b",
+  },
+  {
+    question: "Who created Java?",
+    answers: {
+      a: "Steve Jobs",
+      b: "Bill Gates",
+      c: "James Gosling",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "What is the purpose of the 'public static void main' method in Java?",
+    answers: {
+      a: "To print the contents of a file to the console",
+      b: "To start the execution of a Java program",
+      c: "To declare a variable",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "What is a Java package?",
+    answers: {
+      a: "A collection of related classes and interfaces",
+      b: "A type of data structure",
+      c: "A way to declare a variable",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "What is a Java interface?",
+    answers: {
+      a: "A user interface for a Java program",
+      b: "A type of class that cannot be instantiated",
+      c: "A way to declare aset of methods that a class implementing the interface must implement",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "What is the difference between a Java compiler and a Java interpreter?",
+    answers: {
+      a: "A compiler translates Java code into machine code, while an interpreter executes Java code directly",
+      b: "A compiler executes Java code directly, while an interpreter translates Java code into machine code",
+      c: "There is no difference between a compiler and an interpreter in Java",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "What is the purpose of the 'throws' keyword in Java?",
+    answers: {
+      a: "To declare a new exception class",
+      b: "To catch an exception and handle it",
+      c: "To indicate that a method may throw a specific type of exception",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "What is a Java thread?",
+    answers: {
+      a: "A type of class in Java",
+      b: "A way to handle exceptions in Java",
+      c: "A lightweight process that can run concurrently with other threads",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "What is the difference between an abstract class and an interface in Java?",
+    answers: {
+      a: "An abstract class can be instantiated, while an interface cannot",
+      b: "An abstract class can have method implementations, while an interface cannot",
+      c: "There is no difference between an abstract class and an interface in Java",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "What is the purpose of the 'finally' block in a Java try-catch-finally statement?",
+    answers: {
+      a: "To catch and handle an exception",
+      b: "To execute a block of code regardless of whether an exception was thrown or not",
+      c: "To declare a new exception",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "Psycaro loves sex paa",
+    answers:{
+      a: "True",
+      b: "False",
+      c: "none of "
+    },
+    correctAnswer: "a",
+  },
 
 ];
 
@@ -31,30 +274,26 @@ displayQuestion(questions[currentQuestion]);
 console.log(timer);
 const timeRemainingSpan = document.getElementById("time-remaining");
 const startQuizButton = document.getElementById("start-quiz-button");
-// Setting the initial time remaining for the quiz
-let timeRemaining = 60;
+let timeRemaining = 600; // 10 minutes in seconds
 
-//Creating a function to start timer
-function startTimer(){
+function startTimer() {
   const timer = setInterval(() => {
-    timeRemaining --;
-    timeRemainingSpan.textContent = timeRemaining;
+    timeRemaining--;
+    const minutes = Math.floor(timeRemaining / 60);
+    const seconds = timeRemaining % 60;
+    timeRemainingSpan.textContent = `${minutes}:${seconds.toString().padStart(2, "0")}`;
 
-    if(timeRemaining <= 0) {
+    if (timeRemaining <= 0) {
       clearInterval(timer);
       displayScore();
-      // timeRemainingSpan.textContent = "Quiz Over";
-  
     }
-  }, 1000);
-  
-} 
+  }, 1000); // 1 second interval
+}
 
-startQuizButton.addEventListener('click', () => {
-  console.log('button clicked');
+startQuizButton.addEventListener("click", () => {
+  console.log("button clicked");
   startTimer();
 });
-
 
   
 
